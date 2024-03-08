0e6a4bfb6cc677957425f7542c6294dc0707cea1
asecer79 <asecer79@DESKTOP-TPOST9I>
2024-03-08 10:08:19 +0300

    u

---
+++ W03/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W03.Models;
+
+namespace W03.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public StudentsController()
+        {
+            SchoolDb.InitializeDb(50);
+        }
+
+        public IActionResult Index()
+        {
+            var students = SchoolDb.Students;
+            return View(students);
+        }
+    }
+}
---
+++ W03/Models/SchoolDb.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+
+namespace W03.Models
+{
+    //Mock database, only for simulation purpose
+    public static class SchoolDb
+    {
+        public static List<Student> Students = new List<Student>();
+        public static void InitializeDb(int number)
+        {
+            if (Students.Count==0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    var student = new Faker<Student>()
+                        .RuleFor(s => s.Id, f => f.IndexFaker + 1)
+                        .RuleFor(s => s.Name, f => f.Name.FullName())
+                        .RuleFor(s => s.Email, (f, s) => f.Internet.Email(s.Name))
+                        .RuleFor(s => s.Age, f => f.Random.Int(18, 38));
+
+
+                    Students.Add(student);
+
+                }
+
+            }
+            
+        }
+    }
+}
